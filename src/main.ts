626e61628ceb4168a239acc75a20bcf2356b3f00
lee <1079811936@qq.com>
2021-06-18 21:34:11 +0800

    创建三个view路由并创建全局组件Nav

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Nav from '@/components/Nav.vue'//引入Nav
 
 Vue.config.productionTip = false
+Vue.component('Nav',Nav)//全局组件Nav
 
 new Vue({
   router,
---
+++ src/views/Money.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Money.vue
+        <Nav/>
+        <!-- 引用Nav组件 -->
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
