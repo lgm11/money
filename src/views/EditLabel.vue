4146668f618b16afbc45544c337ae7a3224dc491
lee <1079811936@qq.com>
2021-06-24 18:07:11 +0800

    点击标签可跳转到编辑页面并且路由为各自的id

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -5,11 +5,22 @@
 </template>
 
 <script lang="ts">
+import tagListModel from '@/model/tagListModel';
 import Vue from 'vue';
 import { Component} from 'vue-property-decorator';
 @Component
     export default class Labels extends Vue{
-        
+        created(){
+            const id = this.$route.params.id
+            tagListModel.fetch()
+            const tags = tagListModel.data
+            const tag = tags.filter(t => t.id === id)[0]
+            if(tag){
+
+            }else{
+                this.$router.replace('/404')
+            }
+        }
     }
 </script>
 
