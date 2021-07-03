53b1b75a71b8f74ba4f7be2003b5e432f0c517c6
lee <1079811936@qq.com>
2021-07-03 19:10:25 +0800

    限制页面宽度1

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd dist
 
 git init
 git add -A
-git commit -m '限制页面宽度'
+git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
